8c336731fcb3124fc30c150053938930398f97e4
Stanislav <stanislav_kharchenko@outlook.com>
2023-06-11 05:47:11 +0200

    started

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,20 +1,24 @@
 "use client";
 
-import {CustomButtonProps} from "@/types";
+import { CustomButtonProps } from "@/types";
 
-const CustomButton = ({title, containerStyle, handleClick}: CustomButtonProps) => {
-    return (
-        <div>
-            <button disabled={false}
-                    type={"button"}
-                    className={`custom-btn ${containerStyle}`}
-                    onClick={handleClick}>
-                <span className={"flex-1"}>
-                    {title}
-                </span>
-            </button>
-        </div>
-    );
+const CustomButton = ({
+  title,
+  containerStyle,
+  handleClick,
+}: CustomButtonProps) => {
+  return (
+    <div>
+      <button
+        disabled={false}
+        type={"button"}
+        className={`custom-btn ${containerStyle}`}
+        onClick={handleClick}
+      >
+        <span className={"flex-1"}>{title}</span>
+      </button>
+    </div>
+  );
 };
 
 export default CustomButton;
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,31 +1,39 @@
 "use client";
 import Image from "next/image";
-import {CustomButton} from "@/components/index";
+import { CustomButton } from "@/components/index";
 const Hero = () => {
-    const handleScroll = () => {}
+  const handleScroll = () => {};
 
-    return (
-        <div className="hero">
-            <div className="flex-1 pt-36 padding-x">
-                <h1 className="hero__title">Find, book, rent a car - quickly and easily!</h1>
-                <p className="hero__subtitle">
-                    Streamline your car rental experience with our effortless booking process
-                </p>
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Find, book, rent a car - quickly and easily!
+        </h1>
+        <p className="hero__subtitle">
+          Streamline your car rental experience with our effortless booking
+          process
+        </p>
 
-                <CustomButton title="Explore Cars"
-                              containerStyle="bg-primary-blue text-white rounded-full mt-10"
-                              handleClick={handleScroll}
-                />
-                <div className="hero__image-container">
-                    <div className="hero__image">
-                        <Image src="/hero.png" className="object-contain" alt="hero-image"
-                              fill />
-                        <div className="hero__image-overlay"/>
-                    </div>
-                </div>
-            </div>
+        <CustomButton
+          title="Explore Cars"
+          containerStyle="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+        <div className="hero__image-container">
+          <div className="hero__image">
+            <Image
+              src="/hero.png"
+              className="object-contain"
+              alt="hero-image"
+              fill
+            />
+            <div className="hero__image-overlay" />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
