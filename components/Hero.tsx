dada1a2a566d3f5ae6935b68746780e970bad62a
Kharchenko Stanislav <stanislav_kharchenko@outlook.com>
2023-06-13 16:47:22 +0200

    start

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,18 +1,27 @@
 "use client";
+
 import Image from "next/image";
 import { CustomButton } from "@/components/index";
+
 const Hero = () => {
-  const handleScroll = () => {};
+  const handleScroll = () => {
+    const nextSection = document.getElementById("discover");
+
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
-          Find, book, rent a car - quickly and easily!
+          Find, book, rent a car—quick and super easy!
         </h1>
+
         <p className="hero__subtitle">
           Streamline your car rental experience with our effortless booking
-          process
+          process.
         </p>
 
         <CustomButton
@@ -20,17 +29,13 @@ const Hero = () => {
           containerStyle="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
         />
-        <div className="hero__image-container">
-          <div className="hero__image">
-            <Image
-              src="/hero.png"
-              className="object-contain"
-              alt="hero-image"
-              fill
-            />
-          </div>
-          <div className="hero__image-overlay" />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
         </div>
+
+        <div className="hero__image-overlay" />
       </div>
     </div>
   );
